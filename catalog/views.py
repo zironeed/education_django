f2314ffac04dd9774622e6c99a89a719ac15e9d2
Adil S <forever@nzironeed.ru>
2023-07-04 02:31:02 +0500

    20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/base.html')
 
 
 def contacts(request):
@@ -13,3 +14,11 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'Вам письмо, вот. От {name} ({phone}): \n{message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product(request):
+    products = Product.objects.all()
+    context = {
+        'product_list': products
+    }
+    return render(request, 'catalog/product.html', context)
--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path("product/", product),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
