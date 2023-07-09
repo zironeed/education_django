3f1c8b8ba02a17eaddf8d68491724eccbb6bd269
Adil S <forever@nzironeed.ru>
2023-07-09 21:08:31 +0500

    22.1 (done)

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     banned_words = ['казино', 'криптовалют', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
     class Meta:
@@ -29,7 +36,7 @@ class ProductForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Version
